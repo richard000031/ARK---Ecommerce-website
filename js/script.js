a66303f28576bbb5c11cac3dcabe20b74861535c
richard000031 <richard000031@gmail.com>
2023-02-20 10:11:20 +0800

    update marquee js

--- js/script.js
+++ js/script.js
@@ -13,16 +13,3 @@ if (close) {
         nav.classList.remove('active');
     })
 }
-
-
-
-// index.marquee
-const root = document.documentElement;
-const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
-const marqueeContent = document.querySelector("ul.marquee-content");
-
-root.style.setProperty("--marquee-elements", marqueeContent.children.length);
-
-for(let i=0; i<marqueeElementsDisplayed; i++) {
-  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
-}
